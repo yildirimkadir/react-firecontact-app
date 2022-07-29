3d6704dea37a55c3b750309fcfb4bd99b8d590e7
yildirimkadir <yildirimkad34@gmail.com>
2022-07-29 23:56:02 +0200

    "updated"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
 import './App.css';
+import Contact from './components/Contact';
+import Register from './components/Register';
 
 function App() {
   return (
     <div className="App">
+      <Contact />
+      <Register />
     </div>
   );
 }
